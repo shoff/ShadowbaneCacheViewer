a0627a76f99e8ec88291a036f1db63f9339e781a
Steve <shoff@manageamerica.com>
2021-04-30 20:22:32 -0500

    Starts adding in model parsers (this is the part that the legacy code didn't test)

---
+++ src/Shadowbane.Cache/CacheIndexLookup.cs
@@ -0,0 +1,21 @@
+﻿namespace Shadowbane.Cache
+{
+    using System.Collections.Generic;
+    using CacheTypes;
+
+    // contains a list of valid ids for each cacheFile type
+    public class CacheIndexLookup : Dictionary<CacheFile, List<uint>>
+    {
+        public bool IsValidId(CacheFile cacheFile, uint identity)
+        {
+            if (!this.ContainsKey(cacheFile))
+            {
+                return false;
+            }
+
+            return this[cacheFile].Contains(identity);
+        }
+    }
+
+ 
+}
