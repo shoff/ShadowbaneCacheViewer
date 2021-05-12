bbc10f22681339d26b41a9f7d09231ce23e9d15a
Steve <smhoff256@gmail.com>
2021-05-12 11:26:05 -0500

    Adds tests for CacheObjectBuilder Makes MeshExporter static Fixes naming of folders Adds .sbmesh and .sbri file types. Fixes mesh.bt 010 template

---
+++ src/Shadowbane.Cache/InvalidMeshException.cs
@@ -0,0 +1,16 @@
+﻿namespace Shadowbane.Cache
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public sealed class InvalidMeshException : ApplicationException
+    {
+        public InvalidMeshException(string message)
+            : base(message){}
+        private InvalidMeshException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
