ae908d73486810598d58513704724be641412e90
Steve <smhoff256@gmail.com>
2023-05-22 14:01:08 -0500

    Unity work

---
+++ src/Arcane.Cache/Config/Locations.cs
@@ -0,0 +1,14 @@
+﻿namespace Arcane.Cache.Config;
+
+public static class Locations
+{
+    public const string ZONE_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\CZONE"; // yuck
+    public const string RENDER_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\RENDER"; // yuck
+    public const string STRUCTURE_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\COBJECTS\\STRUCTURE"; // yuck
+    public const string ASSET_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\COBJECTS\\ASSET"; // yuck
+    public const string ASSET_STRUCTURE_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\COBJECTS\\ASSETSTRUCTURE"; // yuck
+    public const string RUNE_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\COBJECTS\\RUNE"; // yuck
+    public const string TEXTURE_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\TEXTURE"; // yuck
+    public const string MESH_PATH = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP\\MESH"; // yuck
+    public const string ARCANE_DUMP = "C:\\dev\\ShadowbaneCacheViewer\\ARCANE_DUMP"; // yuck
+}
